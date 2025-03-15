f6d4a026697fd6316f71480ed88e7aeeca9a2cf4
Varun Nair <nairvarun104@gmail.com>
2025-03-16 03:25:43 +0530

    .

--- onefetch/.bashrc.d/onefetch.sh
+++ onefetch/.bashrc.d/onefetch.sh
@@ -3,12 +3,13 @@
 # disabled-fields: [possible values: project, description, head, pending, version, created, languages, dependencies, authors, last-change, contributors, url, commits, churn, lines-of-code, size, license]
 
 alias onefetch="onefetch \
-	--disabled-fields head authors churn size \
+	--disabled-fields head churn size \
 	--number-of-languages 9 \
 	--text-colors 4 7 7 4 4 7 \
 	--number-separator comma \
 	--true-color never \
 	--image /home/nv/.local/share/onefetch/rei.jpg \
 	--image-protocol sixel \
-	--no-color-palette \
-	--nerd-fonts"
+	--no-color-palette"
+
+	# --nerd-fonts"
