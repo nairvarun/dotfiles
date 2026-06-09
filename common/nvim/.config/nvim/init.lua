f9930998b6e43fe96f2f187f5171f039c906e93a
Varun Nair <nairvarun104@gmail.com>
2026-06-10 03:26:08 +0530

    tab width = 2 in nvim

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -1,3 +1,8 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+
 require("catppuccin").setup({
   flavour = "mocha", -- latte, frappe, macchiato, mocha, vim
 })
